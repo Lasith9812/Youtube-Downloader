db7fc7722c6c8a79bd95431084e88c001a3faa77
Lasith9812 <lasiththilina9812@gmail.com>
2022-09-23 11:17:40 +0530

    Project Start

---
+++ tube.sh
@@ -0,0 +1,11 @@
+#!/bin/python3
+from pytube import YouTube
+
+
+
+while True:
+    url = input("Input URL: ")
+    yt = YouTube(url)
+    print(yt.title)
+    vid = yt.streams.get_by_itag(160)
+    vid.download()
